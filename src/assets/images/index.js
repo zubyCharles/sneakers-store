bc4856db33b1667071a04044cc85c5b10f4d2eb7
zubyCharles <zubecharles@gmail.com>
2022-04-30 22:14:25 +0100

    added all assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,39 @@
+import CartIcon from './icon-cart.svg';
+import CloseIcon from './icon-close.svg';
+import DeleteIcon from './icon-delete.svg';
+import MenuIcon from './icon-menu.svg';
+import MinusIcon from './icon-minus.svg';
+import NextIcon from './icon-next.svg';
+import PlusIcon from './icon-plus.svg';
+import PreviousIcon from './icon-previous.svg';
+import AvatarImage from './image-avatar.png';
+import Product1_Thumbnail from './image-product-1-thumbnail.jpg';
+import Product1 from './image-product-1.jpg';
+import Product2_Thumbnail from './image-product-2-thumbnail.jpg';
+import Product2 from './image-product-2.jpg';
+import Product3_Thumbnail from './image-product-3-thumbnail.jpg';
+import Product3 from './image-product-3.jpg';
+import Product4_Thumbnail from './image-product-4-thumbnail.jpg';
+import Product4 from './image-product-4.jpg';
+
+const images = {
+  CartIcon,
+  CloseIcon,
+  DeleteIcon,
+  MenuIcon,
+  MinusIcon,
+  NextIcon,
+  PlusIcon,
+  PreviousIcon,
+  AvatarImage,
+  Product1_Thumbnail,
+  Product1,
+  Product2_Thumbnail,
+  Product2,
+  Product3_Thumbnail,
+  Product3,
+  Product4_Thumbnail,
+  Product4,
+};
+
+export default images;
